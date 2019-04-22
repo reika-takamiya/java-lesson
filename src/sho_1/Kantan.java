d58306e9f798cdcd1eb4ddcb807793f273de4cb8
reika.takamiya <reika.takamiya@esm.co.jp>
2019-04-22 14:05:02 +0900

    p31の問題1-3あげました

--- src/sho_1/Kantan.java
+++ src/sho_1/Kantan.java
@@ -1,4 +1,15 @@
 package sho_1;
 
+/**
+ * 意外にjavaって簡単だ。と表示して終了するプログラム
+ */
+
+
 public class Kantan {
+    public static void main(String[] args){
+        System.out.println("意外に\"java\"って簡単だ。");
+        System.out.println("意外に\"java\"って簡単だ。");
+        System.out.println("意外に\"java\"って簡単だ。");
+    }
+
 }
