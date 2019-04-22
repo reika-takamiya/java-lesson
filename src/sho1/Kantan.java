bf41a6a5fd7ae1599653eaa1f65fc0f4c7299227
reika.takamiya <reika.takamiya@esm.co.jp>
2019-04-22 18:01:36 +0900

    コメント内容と実際に表示される内容を正しいほうに修正しました。 中カッコの空行を削除しました。

--- src/sho1/Kantan.java
+++ src/sho1/Kantan.java
@@ -1,7 +1,7 @@
 package sho1;
 
 /**
- * 意外にjavaって簡単だ。と表示して終了するプログラム
+ * 意外に"java"って簡単だ。と表示して終了するプログラム
  */
 public class Kantan {
     public static void main(String[] args) {
@@ -9,5 +9,4 @@ public class Kantan {
         System.out.println("意外に\"java\"って簡単だ。");
         System.out.println("意外に\"java\"って簡単だ。");
     }
-
 }
