39a8504bfe2029d77983d4383d615daa5442243f
reika.takamiya <reika.takamiya@esm.co.jp>
2019-04-22 14:05:32 +0900

    p32の問題1-4あげました

--- src/sho_1/Aisatsu2.java
+++ src/sho_1/Aisatsu2.java
@@ -1,4 +1,10 @@
 package sho_1;
 
-public class Aisatsu2 {
+/**
+ *Aisatsu.javaを修正してSystem.out.printlnを一度だけ使用して同じ結果にする
+ */
+    public class Aisatsu2 {
+        public static void main (String[] args){
+            System.out.println("おはよう。\nこんにちは。\nこんばんは。");
+    }
 }
