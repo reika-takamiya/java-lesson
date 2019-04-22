07bdb2dc00803827b79094b9805581e8ccf90eb9
reika.takamiya <reika.takamiya@esm.co.jp>
2019-04-22 14:03:44 +0900

    list1-2あげました

--- src/sho_1/Aisatsu.java
+++ src/sho_1/Aisatsu.java
@@ -1,4 +1,12 @@
 package sho_1;
 
+/**
+ * 画面にあいさつを表示して終了するプログラム
+ */
 public class Aisatsu {
+    public static void main (String[] args){
+        System.out.println("おはよう。");
+        System.out.println("こんにちは。");
+        System.out.println("こんばんは。");
+    }
 }
