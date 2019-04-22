c9713e0405219235ddb9e56c0c2028724e86f010
reika.takamiya <reika.takamiya@esm.co.jp>
2019-04-22 17:56:30 +0900

    *のあとの半角スペース追加しました 改行変更しました mainの後の半角スペース削除しました

--- src/sho1/Aisatsu2.java
+++ src/sho1/Aisatsu2.java
@@ -1,10 +1,10 @@
 package sho1;
 
 /**
- *Aisatsu.javaを修正してSystem.out.printlnを一度だけ使用して同じ結果にする
+ * Aisatsu.javaを修正してSystem.out.printlnを一度だけ使用して同じ結果にする
  */
 public class Aisatsu2 {
-    public static void main (String[] args) {
+    public static void main(String[] args) {
         System.out.println("おはよう。\nこんにちは。\nこんばんは。");
     }
 }
