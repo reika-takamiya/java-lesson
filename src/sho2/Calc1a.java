3a4a3bc8285480c461838768ea5b742beacf0a42
reika.takamiya <reika.takamiya@esm.co.jp>
2019-04-23 14:19:46 +0900

    voidの前の半角スペースの削除

--- src/sho2/Calc1a.java
+++ src/sho2/Calc1a.java
@@ -4,7 +4,7 @@ package sho2;
  * printfのメソッドの例
  */
 public class Calc1a {
-    public static  void main(String[] args) {
+    public static void main(String[] args) {
         System.out.printf("加算の結果は%dです。 %n", 3 + 2);
         System.out.printf("減算の結果は%dです。 %n", 5 - 8);
         System.out.printf("乗算の結果は%dです。 %n", 3 * 4);
