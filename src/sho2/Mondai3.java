e1e8d24213c21c0df81736d271b3ac3fbba01fea
reika.takamiya <reika.takamiya@esm.co.jp>
2019-04-23 13:07:44 +0900

    p47 問題2-3作成しました

--- src/sho2/Mondai3.java
+++ src/sho2/Mondai3.java
@@ -1,4 +1,10 @@
 package sho2;
 
+/**
+ * 上辺が2センチ、下辺が3センチ、高さが4センチの台形の面積を計算するプログラムを作成。
+ */
 public class Mondai3 {
+    public static void main(String[] args) {
+        System.out.println((2 + 3) * 4 / 2);
+    }
 }
