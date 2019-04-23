0130eb76c0f161d50667ce2c34c11f5d5e32d5ac
reika.takamiya <reika.takamiya@esm.co.jp>
2019-04-23 14:18:57 +0900

    mainの前の半角スペースの削除

--- src/sho2/Mondai4.java
+++ src/sho2/Mondai4.java
@@ -12,7 +12,7 @@ package sho2;
  *
  */
 public class Mondai4 {
-    public static void  main(String[] args) {
+    public static void main(String[] args) {
         System.out.println(100000L * 100000L);
     }
 }
