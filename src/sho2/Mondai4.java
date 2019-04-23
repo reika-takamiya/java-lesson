23c0a7de64f7cdbb02549e8358bfa149f3476f10
reika.takamiya <reika.takamiya@esm.co.jp>
2019-04-23 13:08:00 +0900

    p47 問題2-4作成しました

--- src/sho2/Mondai4.java
+++ src/sho2/Mondai4.java
@@ -1,4 +1,18 @@
 package sho2;
 
+/**
+ * 下のプログラムを実行したところ10000000000ではなく410065408と表示されてしまった。
+ * 10000000000と表示させるためのプログラムを作成する。
+ *
+ * public class CalcBig {
+ *     public static void main(String[] args) {
+ *         System.out.println(100000 * 100000);
+ *     }
+ * }
+ *
+ */
 public class Mondai4 {
+    public static void  main(String[] args) {
+        System.out.println(100000L * 100000L);
+    }
 }
