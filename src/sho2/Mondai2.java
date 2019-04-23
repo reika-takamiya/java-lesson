26bfc489b3fbe829d17d3f02de5ad0a32869e87b
reika.takamiya <reika.takamiya@esm.co.jp>
2019-04-23 13:07:22 +0900

    p46 問題2-2作成しました

--- src/sho2/Mondai2.java
+++ src/sho2/Mondai2.java
@@ -1,4 +1,20 @@
 package sho2;
 
+/**
+ * 次の表を完成させるプログラムを作成する。ただし計算はプログラムにさせる。
+ */
 public class Mondai2 {
+    public static void main(String[] args) {
+        System.out.println("0 × 0 = " + (0 * 0));
+        System.out.println("0 × 0 = " + (1 * 1));
+        System.out.println("0 × 0 = " + (2 * 2));
+        System.out.println("0 × 0 = " + (3 * 3));
+        System.out.println("0 × 0 = " + (4 * 4));
+        System.out.println("0 × 0 = " + (5 * 5));
+        System.out.println("0 × 0 = " + (6 * 6));
+        System.out.println("0 × 0 = " + (7 * 7));
+        System.out.println("0 × 0 = " + (8 * 8));
+        System.out.println("0 × 0 = " + (9 * 9));
+        System.out.println("0 × 0 = " + (10 * 10));
+    }
 }
