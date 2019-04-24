c635b4922c6502724f7828604e71239a17ce5304
reika.takamiya <reika.takamiya@esm.co.jp>
2019-04-24 09:56:25 +0900

    p58 list3-1

---
+++ src/sho3/Var1.java
@@ -0,0 +1,12 @@
+package sho3;
+
+/**
+ * 変数の宣言・代入・参照
+ */
+public class Var1 {
+    public static void main(String[] args) {
+        int x;
+        x = 4;
+        System.out.println(2 * x + 3);
+    }
+}
