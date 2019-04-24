cd62c3564007a987c926db199cd9dff515831d05
reika.takamiya <reika.takamiya@esm.co.jp>
2019-04-24 15:46:19 +0900

    インデントの修正

--- src/sho4/Kasa2.java
+++ src/sho4/Kasa2.java
@@ -13,13 +13,13 @@ public class Kasa2 {
             String line = reader.readLine();
             int n = Integer.parseInt(line);
             System.out.println("降水確率は" + n + "%です。");
-              if (n > 100) {
-                  System.out.println("降水確率は0～100の間ですよ");
-              } else if (n >= 50) {
+            if (n > 100) {
+                System.out.println("降水確率は0～100の間ですよ");
+            } else if (n >= 50) {
                 System.out.println("傘を忘れずにね。");
-              } else {
-                  System.out.println("傘はいりません。");
-              }
+            } else {
+                System.out.println("傘はいりません。");
+            }
             System.out.println("いってらっしゃい。");
         } catch (IOException e) {
             System.out.println(e);
