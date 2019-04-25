2794da0d9014b29ab8ea9451b39c1ae2f0821dff
reika.takamiya <reika.takamiya@esm.co.jp>
2019-04-25 11:23:52 +0900

    p116 list5-4

---
+++ src/sho5/Drink4.java
@@ -0,0 +1,37 @@
+package sho5;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+/**
+ * String型でswitch文を使った例
+ */
+public class Drink4 {
+    public static void main(String[] args) {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        try {
+            System.out.println("飲み物は何が好きですか？");
+            System.out.println("オレンジジュース(orange)");
+            System.out.println("コーヒー(coffee)");
+            System.out.println("どちらでもない(other)");
+            System.out.println("orange,coffee,otherのどれかを選んでください。");
+            String moji = reader.readLine();
+            switch (moji) {
+                case "orange":
+                    System.out.println("オレンジジュースです。");
+                    break;
+
+                case "coffee":
+                    System.out.println("コーヒーです。");
+                    break;
+
+                default:
+                    System.out.println("どちらでもありません。");
+                    break;
+            }
+        } catch (IOException e) {
+            System.out.println(e);
+        }
+    }
+}
