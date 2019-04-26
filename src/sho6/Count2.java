d7cede83971ce4f7c7f75c1ab78886836ae1b1dd
reika.takamiya <reika.takamiya@esm.co.jp>
2019-04-26 10:26:39 +0900

    変数名の修正

--- src/sho6/Count2.java
+++ src/sho6/Count2.java
@@ -5,8 +5,8 @@ package sho6;
  */
 public class Count2 {
     public static void main(String[] args) {
-        for (int number = 0; number < 3; number++) {
-            System.out.println(number);
+        for (int i = 0; i < 3; i++) {
+            System.out.println(i);
         }
         System.out.println("end");
     }
