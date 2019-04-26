fde4dd44060e8dcf48acc3b239d033ff055676cb
reika.takamiya <reika.takamiya@esm.co.jp>
2019-04-26 17:21:21 +0900

    正しい実行結果になるように修正。

--- src/sho6/Mondai4.java
+++ src/sho6/Mondai4.java
@@ -6,7 +6,6 @@ package sho6;
 public class Mondai4 {
     public static void main(String[] args) {
         for (int i = 1; i < 10; i++) {
-            System.out.println(i + ":");
             for (int j = 1; j < 10; j++) {
                 System.out.print(i + "*" + j + "=" + i * j + ",");
             }
