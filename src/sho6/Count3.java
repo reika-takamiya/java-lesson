5b84bd42006a72d906e88d52817feba4758ac5de
reika.takamiya <reika.takamiya@esm.co.jp>
2019-04-26 10:27:16 +0900

    Math.powを使って作成

--- src/sho6/Count3.java
+++ src/sho6/Count3.java
@@ -5,10 +5,12 @@ package sho6;
  */
 public class Count3 {
     public static void main(String[] args) {
-        for (int number = 0; number < 10; number++) {
-            System.out.println(number + "の2乗は" + (number * number) + "で、");
-            System.out.println("3乗は" + (number * number * number) + "です。");
+        for (int i = 0; i < 10; i++) {
+            double resultOfPower1 = Math.pow(i, 2);
+            double resultOfPower2 = Math.pow(i, 3);
+            System.out.println(i + "の2乗は" + resultOfPower1 + "で、");
+            System.out.println("3乗は" + resultOfPower2 + "です。");
+            System.out.println("end");
         }
-        System.out.println("end");
     }
 }
