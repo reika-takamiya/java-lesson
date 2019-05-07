487f7f5c01afdcac2ec524c4244bbc36133c564f
reika.takamiya <reika.takamiya@esm.co.jp>
2019-05-07 16:21:27 +0900

    p150 list7-1

---
+++ src/sho7/While1.java
@@ -0,0 +1,15 @@
+package sho7;
+
+/**
+ * whileの例
+ */
+public class While1 {
+    public static void main(String[] args) {
+        int i = 0;
+        while (i < 3) {
+            System.out.println(i);
+            i++;
+        }
+        System.out.println("end");
+    }
+}
