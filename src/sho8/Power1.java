3711b7b9d7d68cac29e4685847a3aff72f251b5e
reika.takamiya <reika.takamiya@esm.co.jp>
2019-05-08 10:33:27 +0900

    p198 list8-4

---
+++ src/sho8/Power1.java
@@ -0,0 +1,18 @@
+package sho8;
+
+/**
+ * 引数が2つのメソッド
+ */
+public class Power1 {
+    public static void main(String[] args) {
+        System.out.println(getPower(8,2));
+    }
+    /*xのn乗の計算*/
+    public static int getPower(int number, int square) {
+        int y = 1;
+        for (int i = 0; i < square; i++) {
+            y = y * number;
+        }
+        return y;
+    }
+}
