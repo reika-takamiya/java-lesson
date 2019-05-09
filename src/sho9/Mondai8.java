22dbc27827765dbf693e286400f6285c3c268755
reika.takamiya <reika.takamiya@esm.co.jp>
2019-05-09 15:48:55 +0900

    p241 list9-13

---
+++ src/sho9/Mondai8.java
@@ -0,0 +1,27 @@
+package sho9;
+
+public class Mondai8 {
+    public static void main(String[] args) {
+        int[] data = {31, 41, 59, 26, 53, 58, 97, 93, 23, 84};
+        System.out.println("並べる前");
+        for (int i = 0; i < data.length; i++) {
+            System.out.print(data[i] + " ");
+        }
+        System.out.println("");
+        for (int i = 0; i < data.length - 1; i++) {
+            for (int j = i + 1; j < data.length; j++) {
+                if (data[i] > data[j]) {
+                    //交換
+                    int a = data[i];
+                    data[i] = data[j];
+                    data[j] = a;
+                }
+            }
+        }
+        System.out.println("並び替えた後");
+        for (int i = 0; i < data.length; i++) {
+            System.out.print(data[i] + " ");
+        }
+        System.out.println("");
+    }
+}
