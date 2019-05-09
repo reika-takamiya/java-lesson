b8213de5af38b47fd1d5d7e36b17c79c3e18c4f8
reika.takamiya <reika.takamiya@esm.co.jp>
2019-05-09 15:45:53 +0900

    p228 list9-7

---
+++ src/sho9/ShowArgs.java
@@ -0,0 +1,13 @@
+package sho9;
+
+/**
+ * コマンドラインの引数を表示する
+ */
+public class ShowArgs {
+    public static void main(String[] args) {
+        System.out.println("args.lengthの値は" + args.length);
+        for (int i = 0; i < args.length; i++) {
+            System.out.println("args[" + i + "]の値は" + args[i] + "です。");
+        }
+    }
+}
