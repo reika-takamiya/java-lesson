c251a72d910d4a069445887d10fbf6a83f92c996
reika.takamiya <reika.takamiya@esm.co.jp>
2019-05-15 11:45:41 +0900

    p40 問題11‐６

---
+++ src/sho11/Rectangle21.java
@@ -0,0 +1,45 @@
+package sho11;
+
+/**
+ * 問題11-6　list11-21
+ * 引数を一つにする
+ *
+ * <pre>{@code
+ * class Rectangle21 {
+ *     ・・・
+ *     boolean areSame(Rectangle21 a, Rectangle b) {
+ *         if (a ==null || b == null) {
+ *             return false;
+ *         } else if (a.width == b.width && a.height == b.height) {
+ *             return true;
+ *         } else {
+ *             return false;
+ *         }
+ *     }
+ * }
+ * }
+ * </pre>
+ */
+public class Rectangle21 {
+    int width;
+    int height;
+
+    Rectangle21(int width, int height) {
+        setSize(width, height);
+    }
+
+    void setSize(int width, int height) {
+        this.width = width;
+        this.height = height;
+    }
+    public boolean equals(Rectangle21 rectangle) {
+        if (rectangle == null) {
+            return false;
+        } else if (width == rectangle.width && height == rectangle.height) {
+            return true;
+        } else {
+            return false;
+        }
+
+    }
+}
