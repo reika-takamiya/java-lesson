aaad2bc639c35748b4714a8ee517408a861f2bfd
reika.takamiya <reika.takamiya@esm.co.jp>
2019-05-17 13:22:41 +0900

    p102 問題13‐4

---
+++ src/sho13/FactorialTest.java
@@ -0,0 +1,35 @@
+package sho13;
+
+import java.io.IOException;
+
+/**
+ * 問題13－４
+ * 下のプログラムの誤りを直す
+ *
+ * <pre>{@code
+ * public class FactorialTest {
+ *     public static void main(String[] args) {
+ *         System.out.println(factorial (10));
+ *     }
+ *     public static int factorial(int n) {
+ *         return n * factorial(n -1);
+ *     }
+ * }
+ * }
+ * </pre>
+ */
+public class FactorialTest {
+    public static void main(String[] args) {
+        System.out.println(factorial(10));
+    }
+
+    public static int factorial(int n) {
+        if (n <= 0) {
+            return 1;
+        } else {
+            return n * factorial(n - 1);
+        }
+    }
+
+}
+
