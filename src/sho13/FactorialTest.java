2262aefaa8b505cf3e4f1302443df27b7d0e3435
reika.takamiya <reika.takamiya@esm.co.jp>
2019-05-20 17:27:46 +0900

    例外処理を追加した

--- src/sho13/FactorialTest.java
+++ src/sho13/FactorialTest.java
@@ -20,16 +20,21 @@ import java.io.IOException;
  */
 public class FactorialTest {
     public static void main(String[] args) {
-        System.out.println(factorial(10));
-    }
-
-    public static int factorial(int n) {
-        if (n <= 0) {
-            return 1;
-        } else {
-            return n * factorial(n - 1);
+        try {
+            System.out.println(factorial(10));
+        } catch (Exception e) {
+            System.out.println(e);
         }
     }
 
-}
+    public static int factorial(int n) throws Exception {
 
+        if (n > 0) {
+            return n * factorial(n - 1);
+        } else if (n == 0) {
+            return 1;
+        } else {
+            throw new Exception("負の値は指定できません。");
+        }
+    }
+}
