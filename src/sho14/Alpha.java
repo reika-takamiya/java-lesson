17ef4d8df59fefecc1f320ccfa5ffff51c165e7e
reika.takamiya <reika.takamiya@esm.co.jp>
2019-05-20 09:56:44 +0900

    p123　問題14‐2

---
+++ src/sho14/Alpha.java
@@ -0,0 +1,7 @@
+package sho14;
+
+/**
+ * 問題14-2
+ */
+public class Alpha {
+}
---
+++ src/sho14/Delta.java
@@ -0,0 +1,10 @@
+package sho14;
+
+/**
+ * 問題14—2
+ * クラスAlphaをスーパークラスに持ち、インタフェースBeta,Gammaを実装したpublicなクラスDeltaの宣言を書く。
+ * 内容は省略。
+ */
+public class Delta extends Alpha implements Beta,Gamma {
+
+}
