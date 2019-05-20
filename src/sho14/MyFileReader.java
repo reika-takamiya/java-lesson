5b55adc5d7dc5f4c1385651fc9c8f0fb4bc8c95f
reika.takamiya <reika.takamiya@esm.co.jp>
2019-05-20 09:55:20 +0900

    p113 list14-6

---
+++ src/sho14/MyFileReader.java
@@ -0,0 +1,30 @@
+package sho14;
+
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+
+/**
+ * list14-6
+ * MyFileReaderクラス宣言
+ */
+class MyFileReader extends FileReader implements DebugPrintable {
+    String filename = null;
+
+    public MyFileReader(String filename) throws FileNotFoundException {
+        super(filename);
+        this.filename = filename;
+    }
+
+    @Override
+    public void debugPrint() {
+        System.out.println("MyFileReaderのインスタンス：ファイル名は" + filename + "です。");
+    }
+
+    public static void main(String[] args) {
+        try {
+            MyFileReader reader = new MyFileReader("input.txt");
+            reader.debugPrint();
+        } catch (FileNotFoundException e) {
+        }
+    }
+}
