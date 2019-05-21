91ea08137b89f92eb0a379b9ab105a2bb5a915a5
reika.takamiya <reika.takamiya@esm.co.jp>
2019-05-21 11:06:57 +0900

    if文の修正と例外の修正

--- src/sho13/FactorialTest.java
+++ src/sho13/FactorialTest.java
@@ -21,20 +21,20 @@ import java.io.IOException;
 public class FactorialTest {
     public static void main(String[] args) {
         try {
-            System.out.println(factorial(10));
-        } catch (Exception e) {
+            System.out.println(factorial(-4));
+        } catch (IllegalArgumentException e) {
             System.out.println(e);
         }
     }
 
-    public static int factorial(int n) throws Exception {
+    public static int factorial(int n) throws IllegalArgumentException {
 
         if (n > 0) {
             return n * factorial(n - 1);
         } else if (n == 0) {
             return 1;
-        } else {
-            throw new Exception("負の値は指定できません。");
         }
+        throw new IllegalArgumentException("負の値は指定できません。");
     }
+
 }
