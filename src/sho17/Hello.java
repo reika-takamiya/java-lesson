d2cb0d253ba5e69d98821cead605801b8c9a95c9
reika.takamiya <reika.takamiya@esm.co.jp>
2019-05-27 15:03:23 +0900

    p214 ist17-10

---
+++ src/sho17/Hello.java
@@ -0,0 +1,14 @@
+package sho17;
+
+/**
+ * list17-10
+ * java.lang.System.outをスタティックにimportした例
+ */
+
+import static java.lang.System.out;
+
+public class Hello {
+    public static void main(String[] args) {
+        out.println("Hello!");
+    }
+}
