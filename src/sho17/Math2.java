7e1aecb522f82ed92dd17471d5d17cceca7fe9a8
reika.takamiya <reika.takamiya@esm.co.jp>
2019-05-27 15:04:17 +0900

    p216 ist17-12

---
+++ src/sho17/Math2.java
@@ -0,0 +1,19 @@
+package sho17;
+
+/**
+ * list17-12
+ * スタティック・インポート・オン・デマンド宣言の例
+ */
+
+import static java.lang.System.*;
+import static java.lang.Math.*;
+
+public class Math2 {
+    public static void main(String[] args) {
+        int x = 45;
+        int y = 100;
+        int z = max(x, y);
+        out.println("z = " + z);
+        out.println("π = " + PI);
+    }
+}
