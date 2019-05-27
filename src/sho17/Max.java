b50a6a726f2e5466ed82015ba5d5ff96e33a630d
reika.takamiya <reika.takamiya@esm.co.jp>
2019-05-27 15:03:56 +0900

    p215 ist17-11

---
+++ src/sho17/Max.java
@@ -0,0 +1,18 @@
+package sho17;
+
+/**
+ * list17-11
+ * java.lang.Math.maxをスタティックにimportした例
+ */
+
+import static java.lang.System.out;
+import static java.lang.Math.max;
+
+public class Max {
+    public static void main(String[] args) {
+        int x = 45;
+        int y = 100;
+        int z = max(x, y);
+        out.println("z = " + z);
+    }
+}
